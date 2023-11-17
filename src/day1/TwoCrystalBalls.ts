3ff58740fd0d136ecf41c221e7c89f0b56e3e632
agu <iba.agustin@gmail.com>
2023-11-17 14:10:47 -0300

    implemented algo for two crystal balls problem. Finding the most optimal linear search within an array

--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,22 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
+    // establishing optimal jump distances
+    const jmpAmount = Math.floor(Math.sqrt(breaks.length));
 
+    let i = jmpAmount;
+
+    for (; i < breaks.length; i += jmpAmount) {
+        // if we hit a "true", we break
+        if (breaks[i]) {
+            break;
+        }
+    }
+    // Walk back to the previous jump point before we hit true so we can do a linear search
+    i = i - jmpAmount;
+
+    for (let j = 0; j < jmpAmount && i < breaks.length; j++, i++) {
+        if (breaks[i]) {
+            return i;
+        }
+    }
+    return -1;
 }
