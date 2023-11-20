6dcc271d2036c1bac4583098b88c88bd10a888ba
agu <iba.agustin@gmail.com>
2023-11-20 13:08:12 -0300

    implemented bubble sort

--- src/day1/BubbleSort.ts
+++ src/day1/BubbleSort.ts
@@ -1,3 +1,16 @@
 export default function bubble_sort(arr: number[]): void {
+    const length = arr.length;
+    for (let i = 0; i < length; i++) {
+        for (let j = 0; j < length - 1 - i; j++) {
+            if (arr[j] > arr[j + 1]) {
+                swapElements(arr, j);
+            }
+        }
+    }
+}
 
+function swapElements(arr: number[], j: number) {
+    const elem = arr[j];
+    arr[j] = arr[j + 1];
+    arr[j + 1] = elem;
 }
