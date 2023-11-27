3b9017e84b7a800715bd8526db90d71a47f1801e
agu <iba.agustin@gmail.com>
2023-11-27 13:44:05 -0300

    implement partition function for quicksort

--- src/day1/MazeSolver.ts
+++ src/day1/MazeSolver.ts
@@ -1,3 +1,37 @@
-export default function solve(maze: string[], wall: string, start: Point, end: Point): Point[] {
-
+function walk(
+    maze: string[],
+    wall: string,
+    current: Point,
+    end: Point,
+    seen: boolean[][],
+): boolean {
+    // base case
+    // 1. Off the map
+    if (
+        current.x < 0 ||
+        current.x >= maze[0].length ||
+        current.y < 0 ||
+        current.y >= maze.length
+    ) {
+        return false;
+    }
+    // 2. On a wall
+    if (maze[current.y][current.x] === wall) {
+        return false;
+    }
+    // 3. At the end
+    if (current.x === end.x && current.y === end.y) {
+        return true;
+    }
+    // 4.We've seen it
+    if (seen[current.y][current.x]) {
+        return false;
+    }
 }
+
+export default function solve(
+    maze: string[],
+    wall: string,
+    start: Point,
+    end: Point,
+): Point[] {}
--- src/day1/QuickSort.ts
+++ src/day1/QuickSort.ts
@@ -1,3 +1,18 @@
-export default function quick_sort(arr: number[]): void {
+function qs(arr: number[], lo: number, hi: number): void {}
 
+function partition(arr: number[], lo: number, hi: number): number {
+    const pivot = arr[hi];
+
+    let idx = lo - 1;
+
+    for (let i = lo; i < hi; i++) {
+        if (arr[i] <= pivot) {
+            idx++;
+            const tmp = arr[i];
+        }
+    }
+}
+
+export default function quick_sort(arr: number[]): void {
+    qs(arr, 0, arr.length - 1);
 }
